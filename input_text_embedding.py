8914b7256f1b2b4a585a4d18c417d6bb152ca5da
ducanh <ngducanh1223@gmail.com>
2020-05-07 18:33:18 +0700

    Plot 6 8 12 clusters

--- input_text_embedding.py
+++ input_text_embedding.py
@@ -2,12 +2,15 @@ from gensim.models import FastText
 from fse.models import Average
 from fse import IndexedList
 import pandas as pd
-import re
 from sklearn.manifold import TSNE
 from sklearn.cluster import KMeans
 from sklearn.metrics import silhouette_score
 from sklearn.mixture import GaussianMixture
 from sklearn.cluster import AgglomerativeClustering
+import json
+import re
+from ast import literal_eval
+
 
 sentences = []
 sentences2 = []
@@ -18,11 +21,15 @@ short_actions = []
 
 df = pd.read_csv('data/ExpectedConversation_27_04.csv')
 for index, value in df.iterrows():
-    sentence = re.sub('\[|\]|\"|\'', '', str(value['input_text']))
+    text = str(value['input_text'])
+    if "scontent.xx.fbcdn.net" in str(value['input_text']):
+        text = re.sub('\[|\]|\'|\n|\{|\}', '', str(value['cv_outputs']))
+        text = text.split(',')[0].replace('object_type:','')
+    sentence = text
     sentences.append(sentence.split())
     sentences2.append(sentence)
-    short_input_text = str(value['input_text']) if "scontent.xx.fbcdn.net" not in str(
-        value['input_text']) else "url"
+
+    short_input_text = text
     short_bot_text = str(value['bot_text']) if "scontent.xx.fbcdn.net" not in str(value['bot_text']) else "url"
     if len(short_input_text) > 50:
         n = int(len(short_input_text) / 50)
@@ -30,6 +37,7 @@ for index, value in df.iterrows():
     if len(short_bot_text) > 50:
         n = int(len(short_bot_text) / 50)
         short_bot_text = " ".join([short_bot_text[50 * x:50 * (x + 1)] + "-" + "<br>" for x in range(n)])
+
     short_entity = str(value['entities']) if "scontent.xx.fbcdn.net" not in str(value['entities']) else "url"
     short_actions = str(value['action_1']) if "scontent.xx.fbcdn.net" not in str(value['action_1']) else "url"
 
@@ -48,21 +56,33 @@ tsne = TSNE(n_components=2)
 tsne_vectors = tsne.fit_transform(vectors_list)
 
 scores = []
+# for k in range(2,20):
+#     x = k
+#     kmeans = KMeans(n_clusters=x, random_state=0)
+#     kmeans = kmeans.fit(tsne_vectors)
+#     labels = kmeans.labels_
+#     score = silhouette_score(tsne_vectors, labels)
+#     inertia = kmeans.inertia_
+#     scores.append((k, score,inertia))
+#
+# scores_df = pd.DataFrame(scores, columns=['k', 'silhouette_score', 'inertia'])
+# scores_df.to_csv("data/scores_input_text_only.csv", index=False)
+
+gm = GaussianMixture(n_components=6, n_init=10, covariance_type="full").fit(tsne_vectors)
+hc = AgglomerativeClustering(n_clusters=6, affinity='euclidean', linkage='ward').fit_predict(tsne_vectors)
+
 for k in range(2,20):
     x = k
     kmeans = KMeans(n_clusters=x, random_state=0)
     kmeans = kmeans.fit(tsne_vectors)
     labels = kmeans.labels_
-    score = silhouette_score(tsne_vectors, labels)
+    score = silhouette_score(tsne_vectors, gm.predict(tsne_vectors))
     inertia = kmeans.inertia_
     scores.append((k, score,inertia))
 
-scores_df = pd.DataFrame(scores, columns=['k', 'silhouette_score', 'inertia'])
+kmeans = KMeans(n_clusters=6, random_state=0).fit(tsne_vectors)
+scores_df = pd.DataFrame(scores, columns=['k', 'silhouette_score','inertia'])
 scores_df.to_csv("data/scores_input_text_only.csv", index=False)
-
-kmeans = KMeans(n_clusters=8, random_state=0).fit(tsne_vectors)
-gm = GaussianMixture(n_components=8, n_init=10, covariance_type="diag").fit(tsne_vectors)
-hc = AgglomerativeClustering(n_clusters=8, affinity='euclidean', linkage='ward').fit_predict(tsne_vectors)
 
 
 vectors_df = pd.DataFrame(data=tsne_vectors, columns=["x", "y"])
@@ -76,6 +96,6 @@ df['short_input_texts'] = short_input_texts
 df['short_bot_texts'] = short_bot_texts
 df['short_entities'] = short_entities
 
-df.to_csv("data/tsne_vectors_2d_input_text_only.csv", index=False)
+df.to_csv("data/tsne_vectors_2d_input_text_only_6_clusters.csv", index=False)
 # df.to_csv("data/tsne_vectors_3d_input_text_only.csv", index=False)
 x = 0
